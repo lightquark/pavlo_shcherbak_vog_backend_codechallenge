11ee4efa780843f8365b47028dc3b353dd6abe3f
Pavlo Shcherbak <pscherbak@gmail.com>
2020-02-05 00:24:05 +0200

    Solved TestQuestion

---
+++ VogCodeChallenge.Question/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VogCodeChallenge.Question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.TestQuestion();
+        }
+    }
+}
---
+++ VogCodeChallenge.Question/QuestionClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.Question
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            // Original version
+            using (var enumerator = NamesList.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+                }
+            }
+
+            // There are several options to write the similar code in the different way
+
+            // 1. classic for
+            for(int i = 0; i < NamesList.Count; i++)
+            {
+                Console.WriteLine(NamesList[i]);
+            }
+
+            // 2. foreach
+            foreach (string name in NamesList)
+            {
+                Console.WriteLine(name);
+            }
+
+            // 3. linq
+            NamesList.ForEach(name => Console.WriteLine(name));
+        }
+    }
+}
